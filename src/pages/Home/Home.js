b9963a64ed0c506f94d84f8ce8c00e780b889c46
Sohag <sohaghossain7th@gmail.com>
2022-09-26 23:20:06 +0600

    navigate

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,18 @@ import React from 'react';
 import AutoComplete from '../AutoComplete/AutoComplete';
 
 import { autoCompleteData } from "../../data";
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Home = () => {
+    const navigate = useNavigate();
+    useEffect(()=>{
+        if(true){
+            console.log("hi")
+            navigate('/login')
+        }
+    },[])
+    
     return (
         <div className='flex flex-col items-center mt-20 '>
             <AutoComplete  data={autoCompleteData} />
